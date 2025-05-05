5b1a53f0b127ed849b27fda92db5291f9302cb75
luizmourademoura <78830766+luizmourademoura@users.noreply.github.com>
2025-05-05 10:56:26 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,36 @@
-self.addEventListener('install', (event) => {
+const cacheName = 'co2-app-v1';
+const filesToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instalação
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('CO2-Calc-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/co2-calculadora-luiz-moura.html',
-        '/manifest.json',
-        '/icons/icon-192x192.png',
-        '/icons/icon-512x512.png',
-      ]);
+    caches.open(cacheName)
+      .then(cache => cache.addAll(filesToCache))
+  );
+});
+
+// Ativação
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== cacheName)
+          .map(name => caches.delete(name))
+      );
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+// Interceptação de requisições
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
